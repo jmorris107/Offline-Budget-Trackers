64ae6d7a31190d0774fcef1e033c4f281ea565ee
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2020-12-11 07:07:26 -0500

    deploying heroku

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget_db", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/budget_db',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 
 
