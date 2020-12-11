da70c000a429dc290b49eb892d95007a571f156a
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2020-12-11 08:12:15 -0500

    working offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,62 +1,63 @@
 console.log("Hi from your service-worker.js file!");
 
-var CACHE_NAME = "my-site-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-//cache
-var urlsToCache = [
-  "/",
-  "/db.js",
-  "/index.js",
-  "/manifest.json",
-  "/styles.css",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
-];
+// Service Worker install
+self.addEventListener('install', (event) => {
+    console.log('service worker install ...');
+});
 
-self.addEventListener("install", function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-    );
-  });
+// Service Worker activate
+self.addEventListener('activate', (event) => {
+  console.info('activate', event);
+});
 
-  self.addEventListener("fetch", function(event) {
-    // cache all get requests to /api routes
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(event.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request).then(function(response) {
-          if (response) {
-            return response;
-          } else if (event.request.headers.get("accept").includes("text/html")) {
-            // return the cached home page for all requests for html pages
-            return caches.match("/");
-          }
-        });
-      })
-    );
-  });
+self.addEventListener('fetch', (event) => {
+    console.log('service worker fetch ... ' + event.request);
+});
+
+
+
+const version = 'v1';
+
+// インストール時にキャッシュする
+self.addEventListener('install', (event) => {
+  console.log('service worker install ...');
+
+  // キャッシュ完了までインストールが終わらないように待つ
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/index.html',
+        '/index.js',
+        '/styles.css',
+        '/icon_192x192.png',
+        '/icon_512x512.png',
+      ]);
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  console.info('activate', event);
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch', event.request.url);
+
+  event.respondWith(
+    // リクエストに一致するデータがキャッシュにあるかどうか
+    caches.match(event.request).then(function(cacheResponse) {
+      // キャッシュがあればそれを返す、なければリクエストを投げる
+      return cacheResponse || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          // レスポンスをクローンしてキャッシュに入れる
+          cache.put(event.request, response.clone());
+          // オリジナルのレスポンスはそのまま返す
+          return response;
+        });  
+      });
+    })
+  );
+});
