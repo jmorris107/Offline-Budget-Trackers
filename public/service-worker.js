03e9f1287dbe976f8ea289b2720871537b2b3c96
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2020-12-11 08:25:52 -0500

    update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,63 +1,79 @@
 console.log("Hi from your service-worker.js file!");
 
-// Service Worker install
-self.addEventListener('install', (event) => {
-    console.log('service worker install ...');
-});
-
-// Service Worker activate
-self.addEventListener('activate', (event) => {
-  console.info('activate', event);
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('service worker fetch ... ' + event.request);
-});
-
-
-
-const version = 'v1';
-
-// インストール時にキャッシュする
-self.addEventListener('install', (event) => {
-  console.log('service worker install ...');
-
-  // キャッシュ完了までインストールが終わらないように待つ
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/manifest.json',
-        '/index.html',
-        '/index.js',
-        '/styles.css',
-        '/icon_192x192.png',
-        '/icon_512x512.png',
-      ]);
-    })
-  );
-});
-
-
-self.addEventListener('activate', (event) => {
-  console.info('activate', event);
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log('fetch', event.request.url);
-
-  event.respondWith(
-    // リクエストに一致するデータがキャッシュにあるかどうか
-    caches.match(event.request).then(function(cacheResponse) {
-      // キャッシュがあればそれを返す、なければリクエストを投げる
-      return cacheResponse || fetch(event.request).then(function(response) {
-        return caches.open('v1').then(function(cache) {
-          // レスポンスをクローンしてキャッシュに入れる
-          cache.put(event.request, response.clone());
-          // オリジナルのレスポンスはそのまま返す
-          return response;
-        });  
-      });
-    })
-  );
-});
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.json",
+    "/style.css",
+    "/icon_192x192.png",
+    "/icon_512x512.png",
+ 
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
+  
